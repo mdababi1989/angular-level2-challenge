05548c790e1568747c0f763a5edfbd73175e4d5c
mdababi1989 <mdababi1989@gmail.com>
2024-04-05 10:50:39 +0100

    add application navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import {Component} from '@angular/core';
 import {AsyncPipe, JsonPipe} from '@angular/common';
+import {RouterOutlet} from "@angular/router";
+import {NavbarComponent} from "./components/app-navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe],
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-  `,
+  imports: [AsyncPipe, JsonPipe, RouterOutlet, NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   name = 'Angular';
